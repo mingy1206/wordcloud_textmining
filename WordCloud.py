a6801615a56ae9d9e73341a8035103f4e495d191
unknown <mingy837@gmail.com>
2024-03-14 16:48:37 +0900

    mecab and files path to c drive and mecab zip file that has exe

--- WordCloud.py
+++ WordCloud.py
@@ -4,7 +4,8 @@ import subprocess
 from collections import Counter
 import tkinter as tk
 from tkinter import filedialog
-from konlpy.tag import Mecab # 한국어 형태소 분석기 Konlpy의 Mecab 모듈
+from konlpy.tag import Mecab
+
 
 import numpy as np
 import pandas as pd
@@ -114,13 +115,15 @@ def fetch_url_content_selenium(url,count):
 
 # 워드 클라우드를 생성하는 함수입니다.
 def generate_wordcloud(content, font, baseImage, ew, tw):
+
+
     tword = []
     for check in tw:
         if check != '':
             tword.append(check)
             train_word(tword)
 
-    pos_tagged = Mecab("venv/Lib/site-packages/mecab/tools/mecab-ko-dic").pos(content)
+    pos_tagged = Mecab(dicpath = r"C:/mecab/mecab-ko-dic").pos(content)
 
     print("------------------전체 가져온 tag--------------------------")
     print(pos_tagged)
@@ -128,7 +131,7 @@ def generate_wordcloud(content, font, baseImage, ew, tw):
     noun_adj_list = [word for word, tag in pos_tagged if tag in ['NNG', 'SL', 'NNP', 'VA+ETM', 'SH']]
     print(noun_adj_list)
     print("------------------최종 단어 제외 후 tag--------------------------")
-    k_stopword = pd.read_csv('korean_stopword.csv')
+    k_stopword = pd.read_csv('C:/mecab/korean_stopword.csv')
     k_stopword = list(k_stopword['불용어'])
     exclude_words = ew +k_stopword
 
@@ -138,17 +141,17 @@ def generate_wordcloud(content, font, baseImage, ew, tw):
     counts = Counter(filtered_words)
     tags = counts.most_common(1000)
 
-    masking_image = np.array(Image.open("images/{}.png".format(baseImage)))
+    masking_image = np.array(Image.open("C:/mecab/images/{}.png".format(baseImage)))
     print(tags)
 
     # 워드클라우드 인스턴스 설정
     wc = WordCloud(
-        font_path="font/{}/{}.otf".format(font, font),
+        font_path="C:/mecab/font/{}/{}.otf".format(font, font),
         mask=masking_image, # 마스크 이미지를 적용합니다.
         background_color='white',
         max_font_size=300,
         min_font_size=1,
-        max_words=1000
+        max_words=300
     ).generate_from_frequencies(dict(tags))
 
     # 마스크 이미지의 색상을 사용하여 워드 클라우드의 색상을 조정합니다.
@@ -157,14 +160,14 @@ def generate_wordcloud(content, font, baseImage, ew, tw):
     wc = wc.recolor(color_func=image_colors)
 
     # Save to file
-    output_filename = "result/result.png"
+    output_filename = "C:/mecab/result/result.png"
     wc.to_file(output_filename)
     print("파일의 위치를 지정해주세요!")
     save_tags_to_file(tags)
     return {"tags": tags, "image_path": output_filename}
 
 def train_word(tw):
-    with open('venv/Lib/site-packages/mecab/tools/mecab-ko-dic/NNP.csv', "r", encoding='utf-8') as f:
+    with open('C:/mecab/mecab-ko-dic/NNP.csv', "r", encoding='utf-8-sig') as f:
         user_dict = f.readlines()
 
     for word in tw:
@@ -172,12 +175,12 @@ def train_word(tw):
         line = '{},*,*,*,NNP,*,{},{},*,*,*,*,*\n'.format(word, jongsung_TF, word)
         user_dict.append(line)
 
-    with open('venv/Lib/site-packages/mecab/tools/mecab-ko-dic/NNP.csv', 'w', encoding='utf-8') as f:
+    with open('C:/mecab/mecab-ko-dic/NNP.csv', 'w', encoding='utf-8-sig') as f:
         for line in user_dict:
             f.write(line)
     # csv 파일 수정 후 빌드(꼭 필요)
     subprocess.run(["powershell", "Set-ExecutionPolicy", "Unrestricted", "-Force"], input="y", text=True)
-    subprocess.run(["powershell", "cd venv/Lib/site-packages/mecab/tools; echo y | .\\add-userdic-win.ps1"])# add-userdic-win.ps1와 mecab-ko-dic의 위치로 이동
+    subprocess.run(["powershell", "cd C:/mecab; echo y | .\\add-userdic-win.ps1"])# add-userdic-win.ps1와 mecab-ko-dic의 위치로 이동
 
 def get_jongsung_TF(sample_text):
     sample_text_list = list(sample_text)
@@ -194,7 +197,7 @@ def get_jongsung_TF(sample_text):
 
 def save_tags_to_file(tags):
     default_filename = "TextMining.txt"
-    image_filename = "result/result.png"
+    image_filename = "C:/mecab/result/result.png"
 
     root = tk.Tk()
     root.withdraw()
@@ -205,7 +208,7 @@ def save_tags_to_file(tags):
                                              filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
 
     if file_path:
-        with open(file_path, 'w', encoding='utf-8') as file:
+        with open(file_path, 'w', encoding='utf-8-sig') as file:
             for tag in tags:
                 file.write(f"{tag[0]}: {tag[1]}\n")
 
